4241bc1711fb8fdc7c3a57e730cef21fdcdb3f53
Jurian Sluiman <jurian@soflomo.com>
2012-07-03 16:04:41 +0200

    Update license to 3-clause "New BSD"

--- src/SlmCmfKernelDoctrineOrm/Entity/MetaData.php
+++ src/SlmCmfKernelDoctrineOrm/Entity/MetaData.php
@@ -1,4 +1,43 @@
 <?php
+/**
+ * Copyright (c) 2012 Soflomo http://soflomo.com.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Ensemble\KernelDoctrineORM
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2012 Soflomo http://soflomo.com.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://ensemble.github.com
+ */
 
 namespace SlmCmfKernelDoctrineOrm\Entity;
 
@@ -11,7 +50,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @ORM\Table(name="page_metadata")
  */
 class MetaData implements MetaDataInterface
-{ 
+{
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -19,43 +58,43 @@ class MetaData implements MetaDataInterface
      * @var integer
      */
     protected $id;
-    
+
     /**
      * @ORM\OneToOne(targetEntity="SlmCmfKernelDoctrineOrm\Entity\Page", inversedBy="metaData")
      * @var Page
      */
     protected $page;
-    
+
     /**
      * @ORM\Column(type="string")
      * @var string
      */
     protected $title;
-    
+
     /**
      * @ORM\Column(type="string")
      * @var string
      */
     protected $longTitle;
-    
+
     /**
      * @ORM\Column(type="string")
      * @var string
      */
     protected $shortTitle;
-    
+
     /**
      * @ORM\Column(type="string")
      * @var string
      */
     protected $description;
-    
+
     /**
      * @ORM\Column(type="string")
      * @var string
      */
     protected $keywords;
-    
+
     public function getPage()
     {
         return $this->page;
@@ -65,34 +104,34 @@ class MetaData implements MetaDataInterface
     {
         $this->page = $page;
     }
-    
+
     public function getTitle ()
     {
         return $this->title;
     }
-    
+
     public function setTitle ($title)
     {
         $this->title = (string) $title;
         return $this;
     }
-    
+
     public function getShortTitle ()
     {
         return $this->shortTitle;
     }
-    
+
     public function setShortTitle ($shortTitle)
     {
         $this->shortTitle = (string) $shortTitle;
         return $this;
     }
-    
+
     public function hasShortTitle ()
     {
         return !empty($this->shortTitle);
     }
-    
+
     public function getNavigationTitle()
     {
         if ($this->hasShortTitle()) {
@@ -101,23 +140,23 @@ class MetaData implements MetaDataInterface
             return $this->getTitle();
         }
     }
-    
+
     public function getLongTitle ()
     {
         return $this->longTitle;
     }
-    
+
     public function setLongTitle ($longTitle)
     {
         $this->longTitle = (string) $longTitle;
         return $this;
     }
-    
+
     public function hasLongTitle ()
     {
         return !empty($this->longTitle);
     }
-    
+
     public function getDescriptiveTitle()
     {
         if ($this->hasLongTitle()) {
@@ -126,23 +165,23 @@ class MetaData implements MetaDataInterface
             return $this->getTitle();
         }
     }
-    
+
     public function getDescription ()
     {
         return $this->description;
     }
-    
+
     public function setDescription ($description)
     {
         $this->description = (string) $description;
         return $this;
     }
-    
+
     public function getKeywords ()
     {
         return $this->keywords;
     }
-    
+
     public function setKeywords ($keywords)
     {
         $this->keywords = (string) $keywords;
--- src/SlmCmfKernelDoctrineOrm/Entity/Page.php
+++ src/SlmCmfKernelDoctrineOrm/Entity/Page.php
@@ -1,4 +1,43 @@
 <?php
+/**
+ * Copyright (c) 2012 Soflomo http://soflomo.com.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Ensemble\KernelDoctrineORM
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2012 Soflomo http://soflomo.com.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://ensemble.github.com
+ */
 
 namespace SlmCmfKernelDoctrineOrm\Entity;
 
@@ -57,7 +96,7 @@ class Page implements PageInterface
      * @var integer
      */
     protected $order;
-    
+
     /**
      * @Gedmo\TreeParent
      * @ORM\ManyToOne(targetEntity="SlmCmfKernelDoctrineOrm\Entity\Page", inversedBy="children")
@@ -72,7 +111,7 @@ class Page implements PageInterface
      * @var ArrayCollection
      */
     protected $children;
-    
+
     /**
      * @var PageCollection
      */
@@ -151,12 +190,12 @@ class Page implements PageInterface
     {
         $this->root = $root;
     }
-    
+
     public function getOrder()
     {
         return $this->order;
     }
-    
+
     public function setOrder($order)
     {
         $this->order = $order;
@@ -177,7 +216,7 @@ class Page implements PageInterface
         if (!$this->childrenCollection instanceof PageCollection) {
             $this->childrenCollection = new PageCollection($this->children->toArray());
         }
-        
+
         return $this->childrenCollection;
     }
 
@@ -197,13 +236,13 @@ class Page implements PageInterface
         if (false === $includeParent) {
             return $this->route;
         }
-        
+
         $page  = $this;
         $route = array();
         do {
             $route[] = $page->getRoute(false);
         } while (($page = $page->getParent()) instanceof self);
-        
+
         $route = array_reverse($route);
         return implode('/', $route);
     }
@@ -232,7 +271,7 @@ class Page implements PageInterface
     {
         $this->moduleId = $moduleId;
     }
-    
+
     public function getMetaData()
     {
         return $this->metaData;
